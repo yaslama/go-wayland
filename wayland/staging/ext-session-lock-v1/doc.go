43cfb2e419846274b95bc1f17da1f13245b7fe46
Matthias Kunnen <matthias.kunnen@gmail.com>
2024-12-03 17:03:56 +0100

    Update repos moved to GitLab
    Replaces GitHub .xml protocol links with GiLab.
    
    wayland-protocols links replaced with the following RegEx:
    s: https://raw.githubusercontent.com/wayland-project/wayland-protocols/1.31/(.*)
    r: https://gitlab.freedesktop.org/wayland/wayland-protocols/-/raw/1.31/$1?ref_type=tags
    
    Manually replaced:
    - mesa, repo moved to https://gitlab.freedesktop.org/mesa/mesa
    - wayland client, repo moved to http://gitlab.freedesktop.org/wayland/wayland

--- wayland/staging/ext-session-lock-v1/doc.go
+++ wayland/staging/ext-session-lock-v1/doc.go
@@ -1,3 +1,3 @@
 package ext_session_lock
 
-//go:generate go run github.com/rajveermalviya/go-wayland/cmd/go-wayland-scanner -pkg ext_session_lock -suffix v1 -o ext_session_lock.go -i https://raw.githubusercontent.com/wayland-project/wayland-protocols/1.31/staging/ext-session-lock/ext-session-lock-v1.xml
+//go:generate go run github.com/rajveermalviya/go-wayland/cmd/go-wayland-scanner -pkg ext_session_lock -suffix v1 -o ext_session_lock.go -i https://gitlab.freedesktop.org/wayland/wayland-protocols/-/raw/1.31/staging/ext-session-lock/ext-session-lock-v1.xml?ref_type=tags
--- wayland/unstable/tablet-v2/doc.go
+++ wayland/unstable/tablet-v2/doc.go
@@ -1,3 +1,3 @@
 package tablet
 
-//go:generate go run github.com/rajveermalviya/go-wayland/cmd/go-wayland-scanner -pkg tablet -prefix zwp -suffix v2 -o tablet.go -i https://raw.githubusercontent.com/wayland-project/wayland-protocols/1.31/unstable/tablet/tablet-unstable-v2.xml
+//go:generate go run github.com/rajveermalviya/go-wayland/cmd/go-wayland-scanner -pkg tablet -prefix zwp -suffix v2 -o tablet.go -i https://gitlab.freedesktop.org/wayland/wayland-protocols/-/raw/1.31/unstable/tablet/tablet-unstable-v2.xml?ref_type=tags
