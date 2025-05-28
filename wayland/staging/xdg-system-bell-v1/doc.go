2efd86316f198cea89ae18f0aac796d82ab14657
Yaacov Akiba Slama <ya@slamail.org>
2025-05-28 13:27:54 +0300

    Add xdg-system-bell-v1

---
+++ wayland/staging/xdg-system-bell-v1/doc.go
@@ -0,0 +1,3 @@
+package xdg_system_bell
+
+//go:generate go run github.com/yaslama/go-wayland/cmd/go-wayland-scanner -pkg xdg_system_bell -suffix v1 -o xdg_system_bell.go -i https://gitlab.freedesktop.org/wayland/wayland-protocols/-/raw/1.44/staging/xdg-system-bell/xdg-system-bell-v1.xml?ref_type=tags
